ee7c35bc3641257b9bbc6c7c780f21f73cc7baeb
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-10-27 17:47:24 +0200

    Feat : ajout d'une page de chargement

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+const createLoadingWindow = () => {
+  const loadingWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  loadingWindow.loadFile('./loader/loading.html')
+  return loadingWindow
+}
+
+const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     icon: "./public/assets/logo.png",
     width: 800,
     height: 600,
-    frame: false
+    frame: false,
+    show: false,
+    backgroundColor: "rgb(38, 39, 40)",
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
   mainWindow.maximize()
   mainWindow.loadURL('http://localhost:3000')
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+    loadingWindow.close()
+  })
 }
 
+let loadingWindow
+
 app.whenReady().then(() => {
-  createWindow()
+  loadingWindow = createLoadingWindow()
+  createMainWindow()
+  
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      loadingWindow = createLoadingWindow()
+      createMainWindow()
+    }
   })
 })
 
